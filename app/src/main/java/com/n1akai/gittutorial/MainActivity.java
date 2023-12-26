e23d3040fae945eda48277a6942ff6f6950dbff6
N1AKAI <rakansubs@gmail.com>
2023-12-26 16:06:28 +0100

    remove message 2

--- app/src/main/java/com/n1akai/gittutorial/MainActivity.java
+++ app/src/main/java/com/n1akai/gittutorial/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Message 1
-        // Message 2
     }
 }
