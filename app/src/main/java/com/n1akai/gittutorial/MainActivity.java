0031d005817fb57a7ab08e48b5c045d89aebd025
N1AKAI <rakansubs@gmail.com>
2023-12-26 16:05:58 +0100

    added message 2

--- app/src/main/java/com/n1akai/gittutorial/MainActivity.java
+++ app/src/main/java/com/n1akai/gittutorial/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Message 1
+        // Message 2
     }
 }
